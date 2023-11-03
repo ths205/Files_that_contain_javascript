11b791faaf339c28a8253f0bb9f2f9d17ce9c519
Tiffany Schiff <ths205@alum.lehigh.edu>
2023-11-03 13:12:21 -0400

    Create Lesson4_workshop_services.js

---
+++ Lesson4_workshop_services.js
@@ -0,0 +1,47 @@
+angular.module('Service', [])
+
+	//var self=this;
+	//self.grades_array=[];
+    //self.hw_name=[];
+
+	.service('CalculateService', function(){
+        
+        this.average=function(){
+        	var sum=0;
+        	for(var i=0; i<self.hw_grades.grade.length; i++){
+               sum=self.hw_grades.grade[i];
+
+        	}
+        	this.avg=sum/self.hw_grades.length;
+        }
+
+    })
+
+
+    .factory('GradeReportCard', function(){
+
+    	function GradeReportCard(){
+            var self=this;
+            self.hw_grades=[{'assignment':"", 'grade':""}];
+
+    		this.grade="A";
+    		this.num_grade=100;
+            //this.assign="Hw1";
+
+    	}
+
+    	//GradeReportCard.prototype.setGrade_HW_Num=function(g, a){
+    	//	this.num_grade=g;
+        //    this.assign=a;	
+    	//}
+
+
+    	GradeReportCard.prototype.addGrade_HW = function(a){
+            var self = this;
+            // a = { assignment: 'HW2', grade: 85 }
+            self.hw_grades.push(a);
+    	}	
+    		
+        return GradeReportCard;
+
+    });
